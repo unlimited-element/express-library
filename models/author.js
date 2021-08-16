4ccc348d3833b246278c881b6a9b551865e315df
unlimited-element <73407996+unlimited-element@users.noreply.github.com>
2021-08-16 01:34:57 -0700

    Set up pages for author, booklist, bookinstance and genre

--- models/author.js
+++ models/author.js
@@ -1,25 +1,39 @@
 var mongoose = require('mongoose');
-
 var Schema = mongoose.Schema;
+const {
+  DateTime
+} = require("luxon");
 
-var AuthorSchema = new Schema(
-  {
-    first_name: {type: String, required: true, maxLength: 100},
-    family_name: {type: String, required: true, maxLength: 100},
-    date_of_birth: {type: Date},
-    date_of_death: {type: Date},
-  }
-);
+var AuthorSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true,
+    maxLength: 100
+  },
+  family_name: {
+    type: String,
+    required: true,
+    maxLength: 100
+  },
+  date_of_birth: {
+    type: Date
+  },
+  date_of_death: {
+    type: Date
+  },
+});
 
 // Virtual for author's full name
 AuthorSchema
-.virtual('name')
-.get(function () {
-  return this.family_name + ', ' + this.first_name;
-});
+  .virtual('name')
+  .get(function () {
+    return this.family_name + ', ' + this.first_name;
+  });
 
 // Virtual for author's lifespan
-AuthorSchema.virtual('lifespan').get(function() {
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
   var lifetime_string = '';
   if (this.date_of_birth) {
     lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
@@ -33,11 +47,11 @@ AuthorSchema.virtual('lifespan').get(function() {
 
 // Virtual for author's URL
 AuthorSchema
-.virtual('url')
-.get(function () {
-  return '/catalog/author/' + this._id;
-});
+  .virtual('url')
+  .get(function () {
+    return '/catalog/author/' + this._id;
+  });
+
 
 //Export model
 module.exports = mongoose.model('Author', AuthorSchema);
-
